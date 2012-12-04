36b07c845179452e3d29f08019633cf7c68244b0
Nuno Job <nunojobpinto@gmail.com>
2012-12-04 20:38:22 +0000

    [slugify] adds slugs

--- public/js/slugify.js
+++ public/js/slugify.js
@@ -1,16 +1,57 @@
 (function () {
   function slugify(heading) {
-    heading = heading.replace(/[^-a-zA-Z0-9,&\s]+/g, "");
+    heading = heading.replace(/[^\-a-zA-Z0-9,&\s]+/g, "");
     heading = heading.replace(/-/g, "_");
     heading = heading.replace(/\s/g, "-");
     return heading.toLowerCase();
   }
 
-  $('h1, h2, h3, h4').forEach(function (h) {
-    heading = $(h);
-    anchor = slugify(heading.text());
-    heading
-      .attr('id', anchor)
-      .prepend("<a href='##{archor}' class='anchor'>&para;</a>");
+  $(document).ready(function() {
+    var previous_ident = 0
+      , stack          = []
+      , slug_prefix    = ''
+      , first          = true
+      ;
+
+    $("h1, h2, h3, h4, h5").each(function (i, h) {
+      if(first) {
+        first = false;
+        return;
+      }
+
+      var heading = $(h)
+        , slug    = slugify(heading.text())
+        , ident   = +h.tagName.substring(1)
+        ;
+
+      if(slug === '') {
+        return;
+      }
+
+      if(previous_ident > ident) {
+        stack.pop();
+      }
+      else if(previous_ident === ident) {
+        stack.pop();
+        stack.push([ident, slug]);
+      }
+      else {
+        stack.push([ident, slug]);
+      }
+
+      previous_slug = stack.slice(0, stack.length-1).map(function (elem) {
+        return elem[1];
+      }).join('-')
+
+      if(previous_slug && previous_slug !== '') {
+        slug = previous_slug + '-' + slug;
+      }
+
+      heading
+        .attr('id', slug)
+        .prepend("<a href='#" + slug + "' class='anchor'>&para;</a>&nbsp; ");
+
+      previous_ident = ident;
+    });
   });
 })();
